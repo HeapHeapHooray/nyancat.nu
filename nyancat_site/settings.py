fa497fb6aca6dc623da5e32cf370bd8adfb52a4a
HeapHeapHooray <heapheaphooray@hotmail.com>
2026-04-14 02:53:00 -0300

    Fixes?

--- nyancat_site/settings.py
+++ nyancat_site/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'core.middleware.SecurityHeadersMiddleware',
 ]
 
 ROOT_URLCONF = 'nyancat_site.urls'
