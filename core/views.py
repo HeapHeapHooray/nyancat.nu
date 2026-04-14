b8bfa2ab6a158461960a0e6c1d2f7c5f24947276
HeapHeapHooray <heapheaphooray@hotmail.com>
2026-04-14 02:00:41 -0300

    MP4 to MP3 converter

--- core/views.py
+++ core/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
+
+def converter(request):
+    return render(request, 'core/converter.html')
+
