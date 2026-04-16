ccf64a8d32e026242ce6f1276b353543b36d7b15
HeapHeapHooray <heapheaphooray@hotmail.com>
2026-04-16 01:15:01 -0300

    Implemented Video Downloader.

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('converter/', views.converter, name='converter'),
+    path('downloader/', views.video_downloader, name='video_downloader'),
 ]
