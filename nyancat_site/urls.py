25fbea29eee17f8224337364bac73943e3dd4545
HeapHeapHooray <heapheaphooray@hotmail.com>
2026-04-25 18:09:22 -0300

    Move favicon to staticfiles root for easier Nginx serving

--- nyancat_site/urls.py
+++ nyancat_site/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'core/images/favicon.ico', permanent=True)),
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'favicon.ico', permanent=True)),
     path('', include('core.urls')),
 ]
