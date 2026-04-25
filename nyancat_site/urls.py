118cdea56d4dc01101eaa4e3520cfb9c1d0eac70
HeapHeapHooray <heapheaphooray@hotmail.com>
2026-04-25 18:07:27 -0300

    Fix favicon?

--- nyancat_site/urls.py
+++ nyancat_site/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'core/images/favicon.ico', permanent=True)),
     path('', include('core.urls')),
 ]
